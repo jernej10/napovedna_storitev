61b80eec73dd7544c1351bd1e74176d858d111b1
jernej10 <jernej.ofic@gmail.com>
2024-04-22 09:05:06 +0200

    Fix: validation

--- src/data/run_checkpoint.py
+++ src/data/run_checkpoint.py
@@ -1,6 +1,6 @@
 import great_expectations as ge
 from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
-
+import pandas as pd
 
 
 def main():
@@ -15,6 +15,10 @@ def main():
     #dm = DataManager(data_path="data")
     #current = dm.get_dataframe("processed", "current_data")
     #dm.save("processed", "reference_data", current, override=True)
+
+    current_data = pd.read_csv("data/current_data.csv")
+    reference_data_path = "data/reference_data.csv"
+    current_data.to_csv(reference_data_path, index=False)
 
 if __name__ == "__main__":
     main()
